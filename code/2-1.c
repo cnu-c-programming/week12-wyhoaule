5ccb3366e8b4f8edd13dda42a9143bdf56866724
gdy <153972304+wyhoaule@users.noreply.github.com>
2026-05-22 09:48:13 +0900

    2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 int main(int argc, const char* argv[], const char* envp[]) {
-    FILE* fp = fopen("student.txt", "r");
+    FILE* fp = fopen("student.txt", "w");
 
     while (1) {
         char name[10] = {0};
