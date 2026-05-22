3eb9f538bcdf9ed4f12d75a269cf03c18e642239
gdy <153972304+wyhoaule@users.noreply.github.com>
2026-05-22 09:15:56 +0900

    1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -5,7 +5,7 @@ int main(int argc, const char* argv[]) {
     if (argc < 2) 
         return 0;
 
-    for (int i = 0; i < sizeof(argv); i++) {
+    for (int i = 0; i < argc; ++i) {
     const char* filename = argv[1];
     char buffer[256] = {0};
 
