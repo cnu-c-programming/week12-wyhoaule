ddd63ea626a9e9a4e541249c3a5676eee67cc40f
gdy <153972304+wyhoaule@users.noreply.github.com>
2026-05-22 09:43:48 +0900

    2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -2,7 +2,17 @@
 #include <string.h>
 
 int main(int argc, const char* argv[], const char* envp[]) {
+    FILE* fp = fopen("student.txt", "r");
 
-    return 0;
+    while (1) {
+        char name[10] = {0};
+        int score = 0;
+        scanf("%s", name);
+        if (strcmp(name, "quit") == 0)
+            break;
+        scanf("%d",&score);
+
+
+        fprintf(fp, "%s %d\n", name, score);
+    }
 }
-
