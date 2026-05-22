577b13b11497951314c427aa41a2b164251850f6
gdy <153972304+wyhoaule@users.noreply.github.com>
2026-05-22 09:11:49 +0900

    1-2.c

---
+++ code/1-2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc, const char* argv[]) {
+
+    if (argc < 2) 
+        return 0;
+
+    for (int i = 0; i < sizeof(argv); i++) {
+    const char* filename = argv[1];
+    char buffer[256] = {0};
+
+    FILE* fp = fopen(filename, "r");
+    if (fp == NULL)
+        return 0;
+
+    fgets(buffer, sizeof(buffer), fp);
+    fclose(fp);
+
+    printf("%s\n", buffer);
+    }
+
+    return 0;
+}
