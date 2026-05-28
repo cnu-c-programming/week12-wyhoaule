555879b941fba7dda055a2468569b9e817fbc2b7
gdy <153972304+wyhoaule@users.noreply.github.com>
2026-05-28 14:24:40 +0900

    2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -6,8 +6,26 @@ int main(int argc, const char* argv[]) {
 
     FILE* fp = fopen(argv[1], "r");
 
+    if (fp = NULL) {
+        printf("파일을 열 수 없습니다.\n");
+        return 1;
+    }
+
+    int count[26] = {0};
+    int ch;
+
+    while ((ch = fgetc(fp)) != EOF)  {
+        if (ch >= 'a' && ch <= 'z') {
+            counts[ch - 'a']++;
+        }
+     }
+
+    for (int i = 0; i < 26; i++) {
+        printf("%c: %d\n",  'a' + i, counts[i]);
+    }
 
 
     fclose(fp);
+    return 0;
 }
 
