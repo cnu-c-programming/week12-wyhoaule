332854717d5f0daeccb07a4669d7b1522804c00f
gdy <153972304+wyhoaule@users.noreply.github.com>
2026-05-28 21:49:04 +0900

    2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -1,30 +1,28 @@
 #include <stdio.h>
 
 int main(int argc, const char* argv[]) {
-    if (argc < 2)
+    if (argc < 2) {
         return 0;
+    }
 
     FILE* fp = fopen(argv[1], "r");
-
-    if (fp = NULL) {
-        printf("파일을 열 수 없습니다.\n");
-        return 1;
+    if (fp == NULL) {
+        return 0;
     }
 
     int count[26] = {0};
     int ch;
 
-    while ((ch = fgetc(fp)) != EOF)  {
+    while ((ch = fgetc(fp)) != EOF) {
         if (ch >= 'a' && ch <= 'z') {
-            counts[ch - 'a']++;
+            count[ch - 'a']++;
         }
-     }
-
-    for (int i = 0; i < 26; i++) {
-        printf("%c: %d\n",  'a' + i, counts[i]);
     }
 
+    for (int i = 0; i < 26; i++) {
+        printf("%c: %d\n", 'a' + i, count[i]);
+    }
 
     fclose(fp);
+    return 0;
 }
-
