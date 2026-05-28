c12dbc87d96797c214cd980acb31c9869108f8ca
gdy <153972304+wyhoaule@users.noreply.github.com>
2026-05-28 21:43:55 +0900

    3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -8,9 +8,23 @@ int main(int argc, const char* argv[]) {
         return 0;
     }
 
-
     int sum = 0;
+    int num;
+    char invalid_buf[256];
 
+    while (1) {
+        int res = fscanf(fp, "%d", &num);
+        
+        if (res == 1) {
+            sum += num;
+        } else if (res == 0) {
+            if (fscanf(fp, "%255s", invalid_buf) != EOF) {
+                fprintf(stderr, "invalid input %s\n", invalid_buf);
+            }
+        } else {
+            break;
+        }
+    }
 
     printf("sum: %d\n", sum);
     fclose(fp);
