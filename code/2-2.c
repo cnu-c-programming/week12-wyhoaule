8a4f9ebce44c96789c365cc6af1435a803f3667d
gdy <153972304+wyhoaule@users.noreply.github.com>
2026-05-28 13:59:28 +0900

    2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -11,12 +11,30 @@ int main(int argc, const char* argv[]) {
     int count = 0;
     Student students[64];
 
-
+    if (fp == NULL) {
+        printf("파일을 열 수 없습니다.\n");
+        return 1;
+    }
+    while (!feof(fp)) {
+        if (fscanf(fp, "%s %d", students[count].name, &students[count].score) == 2) {
+            count++;
+        }
+    }
 
     int max = 0;
     float avg = 0;
 
+    int sum = 0;
+    for (int i = 0; i < count; i++) {
+        if (students[i].score > max) {
+            max = students[i].score;
+        }
+        sum += students[i].score;
+    }
 
+    if (count > 0) {
+        avg = (float) sum/ count;
+    }
 
     printf("max: %d\n", max);
     printf("avg: %.2f\n", avg);
