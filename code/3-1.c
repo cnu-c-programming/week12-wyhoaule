34966df4af436532a7c5d5266220266d8316b6a6
gdy <153972304+wyhoaule@users.noreply.github.com>
2026-05-28 21:52:09 +0900

    3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 
 int main(int argc, const char* argv[]) {
-    if (argc < 2) 
+    if (argc < 2) {
         return 0;
-    
-    FILE* fp = fopen(argv[1], "r");
-    if (fp == NULL) {
-        perror("file does not exist");
-        return 1;
     }
 
-    if (fp != NULL) {
-        fclose(fp);
+    FILE* fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        perror("file dose not exists");
+        return 1;
     }
 
     return 0;
