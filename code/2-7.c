86e980e53d71901f9e4337ed2ac34500ba7e9f7c
gdy <153972304+wyhoaule@users.noreply.github.com>
2026-05-28 15:10:48 +0900

    2-7.c

--- code/2-7.c
+++ code/2-7.c
@@ -10,7 +10,27 @@ typedef struct config {
 } Config;
 
 void config_parser(Config* config_ptr) {
+    FILE* fp = fopen("config.txt", "r");
+    if (fp == NULL) {
+        printf("config.txt 파일을 열 수 없습니다.\n");
+        return;
+    }
 
+    char key[64];
+    char value[64];
+    
+    while (fscanf(fp, "%63[^=]=%63s\n", key, value) == 2) {
+        if (strcmp(key, "InputFileName") == 0) {
+            strcpy(config_ptr->InputFileName, value);
+        } else if (strcmp(key, "Options") == 0) {
+            config_ptr->Option = atoi(value);
+        } else if (strcmp(key, "SectionName") == 0) {
+            strcpy(config_ptr->SectionName, value);
+        } else if (strcmp(key, "Address") == 0) {
+        config_ptr->Address = strtoull(value, NULL, 0);
+        }
+    }
+    fclose(fp);
 }
 
 int main(int argc, const char* argv[]) {
