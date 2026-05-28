18e12d1260553f66170e4d1469812ee0a90240e0
gdy <153972304+wyhoaule@users.noreply.github.com>
2026-05-28 15:37:46 +0900

    3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -5,7 +5,14 @@ int main(int argc, const char* argv[]) {
         return 0;
     
     FILE* fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        perror("file does not exist");
+        return 1;
+    }
 
+    if (fp != NULL) {
+        fclose(fp);
+    }
 
     return 0;
 }
