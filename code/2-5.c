ab2094c480446b42582197e5e9933ca01fad5f05
gdy <153972304+wyhoaule@users.noreply.github.com>
2026-05-28 14:37:06 +0900

    2-5.c

--- code/2-5.c
+++ code/2-5.c
@@ -4,6 +4,21 @@ int main(int argc, const char* argv[]) {
     if (argc < 3)
         return 0;
 
+    FILE* fp_src = fopen(argv[1], "r"); 
+    FILE* fp_dst = fopen(argv[2], "w"); 
 
+    if (fp_src == NULL || fp_dst == NULL) {
+        printf("파일을 여는 데 실패했습니다.\n");
+        return 1;
+    }
+    
+    int ch;
+    
+    while ((ch = fgetc(fp_src)) != EOF) {
+        fputc(ch, fp_dst);
+    }
+    fclose(fp_src);
+    fclose(fp_dst);
+
+    return 0;
 }
-
